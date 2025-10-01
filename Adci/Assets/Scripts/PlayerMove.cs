cce59ba871cec3274fd4e5fa0ac8a46382b85706
TMGBNoodle <grovesbenedicttm@hendrix.edu>
2025-10-01 14:10:12 -0500

    Unity project added

---
+++ Adci/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,19 @@
+using UnityEditor.PackageManager.UI;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMove : MonoBehaviour
+{
+    public PlayerInput playerInput;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dirX = 0;
+    }
+}
